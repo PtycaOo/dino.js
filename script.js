ddc2b2d6ee8a4ad28f42a0cc5a689703c18b4892
PtycaOo <Denkuzia5155@gmail.com>
2023-09-06 17:05:18 +0300

    Кривой динозавр

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function jump(){
         dino.classList.remove("jump")
     },300)
 }
+
+let isAlive = setInterval(function(){
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let rockLeft = parseInt(window.getComputedStyle(rock).getPropertyValue("left"));
+    if(rockLeft < 50 && rockLeft > 0 && dinoTop >=140){
+        alert("GameOver!")
+    }
+},10)
