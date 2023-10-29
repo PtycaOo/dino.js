e9e1d030d0014c4570712d35ae1413ab93972d72
PtycaOo <DenKuzia5155@gmail.com>
2023-10-29 11:46:21 +0300

    второй комит для Дз

--- script.js
+++ script.js
@@ -14,6 +14,15 @@ function jump(){
     },300)
 }
 
+function jump(){
+    if(dino.classList != "jump"){
+        dino.classList.add("jump")
+    } 
+    setTimeout(function(){
+        dino.classList.remove("jump")
+    },300)
+}
+
 let isAlive = setInterval(function(){
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let rockLeft = parseInt(window.getComputedStyle(rock).getPropertyValue("left"));
